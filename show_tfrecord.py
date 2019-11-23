76911bdc12d4217b3aa6d7ba38e35ea49d90c90c
William Bernardes Magalhães <william1504@gmail.com>
2019-11-23 14:53:44 -0300

    aug

--- show_tfrecord.py
+++ show_tfrecord.py
@@ -3,5 +3,5 @@ from utils import tfrecords, augmentation, other, visualize
 dataset = tfrecords.read(['./data/segmentation_test.tfrecord'], im_size=512)
 dataset = dataset.map(other.normalize, num_parallel_calls=4)
 
-dataset = augmentation.apply(dataset)
+dataset = augmentation.apply(dataset, types=['crop', 'flip', 'rotate'])
 visualize.plot_dataset(dataset.batch(16))
--- utils/augmentation.py
+++ utils/augmentation.py
@@ -6,10 +6,9 @@ from utils import other
 import random
 
 
-def apply(dataset, im_size=256):
+def apply(dataset, im_size=256, types=['rotate, flip, crop']):
     def rotate(x, y):
-        seed = int(random.random() * 1234.)
-        coin = tf.random.uniform(shape=[], minval=0, maxval=4, dtype=tf.int32, seed=seed)
+        coin = tf.random.uniform(shape=[], minval=0, maxval=4, dtype=tf.int32)
         x = tf.image.rot90(x, coin)
         y = tf.image.rot90(y, coin)
         return x, y
@@ -30,9 +29,15 @@ def apply(dataset, im_size=256):
         y = tf.image.random_crop(y, size=[im_size, im_size, 1], seed=seed)
         return x, y
 
-    augmentations = [crop, flip, rotate]
-    for f in augmentations:
-        dataset = dataset.map(f, num_parallel_calls=4)
+    for t in types:
+        if t == 'rotate':
+            dataset = dataset.map(rotate, num_parallel_calls=4)
+        elif t == 'flip':
+            dataset = dataset.map(flip, num_parallel_calls=4)
+        elif t == 'crop':
+            dataset = dataset.map(crop, num_parallel_calls=4)
+        else:
+            continue
 
     dataset = dataset.map(other.clip01, num_parallel_calls=4)
     return dataset
