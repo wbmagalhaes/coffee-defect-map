48408b8fbfdcd45a5424c7e738d1ece771fb03f8
William Bernardes Magalhães <william1504@gmail.com>
2019-11-23 13:44:16 -0300

    remove dmap

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -4,46 +4,8 @@ import glob
 import json
 
 import numpy as np
-import xml.etree.ElementTree as ET
 
 from utils.labelmap import defect_values
-from utils.density_map import gaussian_kernel
-
-
-def read_xml(xml_path):
-    tree = ET.parse(xml_path)
-    root = tree.getroot()
-
-    filename = root.find('filename').text
-
-    dirname = os.path.dirname(xml_path)
-
-    img_path = os.path.join(dirname, filename)
-    image = cv2.imread(img_path)
-
-    bboxes = []
-    for obj in root.findall('object'):
-        name = obj.find('name').text
-        bndbox = obj.find('bndbox')
-        xmin = float(bndbox.find('xmin').text) / image.shape[1]
-        xmax = float(bndbox.find('xmax').text) / image.shape[1]
-        ymin = float(bndbox.find('ymin').text) / image.shape[0]
-        ymax = float(bndbox.find('ymax').text) / image.shape[0]
-
-        weight = defect_values[name]
-
-        bbox = {
-            'xmin': xmin,
-            'xmax': xmax,
-            'ymin': ymin,
-            'ymax': ymax,
-            'name': name,
-            'weight': weight
-        }
-
-        bboxes.append(bbox)
-
-    return image, bboxes
 
 
 def read_json(addr):
@@ -73,35 +35,11 @@ def read_json(addr):
     return image, shapes
 
 
-def generate_dmap(image, bboxes):
-    im_h = image.shape[0]
-    im_w = image.shape[1]
-
-    dmap = np.zeros((im_h, im_w), np.float32)
-
-    for bbox in bboxes:
-        xmin = bbox['xmin'] * im_w
-        xmax = bbox['xmax'] * im_w
-        ymin = bbox['ymin'] * im_h
-        ymax = bbox['ymax'] * im_h
-
-        x = int(xmin + (xmax - xmin) / 2)
-        y = int(ymin + (ymax - ymin) / 2)
-
-        w = 100  # bbox['weight'] * 100
-
-        s = ((xmax - xmin) + (ymax - ymin)) / 8
-        dmap += gaussian_kernel(center=(x, y), map_size=(im_h, im_w), A=w, sx=s, sy=s)
-
-    return dmap
-
-
 def generate_seg(image, shapes):
     im_h = image.shape[0]
     im_w = image.shape[1]
 
     seg_map = np.zeros_like(image, np.float32)
-    wei_map = np.ones_like(image, np.float32)
 
     for shape in shapes:
         def rescale_point(point):
@@ -116,10 +54,7 @@ def generate_seg(image, shapes):
         cv2.fillPoly(seg_map, [points], (1, 1, 1))
         cv2.polylines(seg_map, [points], True, (0, 0, 0), thickness=2)
 
-        cv2.fillPoly(wei_map, [points], (2, 2, 2))
-        cv2.polylines(wei_map, [points], True, (10, 10, 10), thickness=3)
-
-    return seg_map, wei_map
+    return seg_map
 
 
 def resize_image(image, size):
@@ -144,18 +79,15 @@ def load_json(dirs, final_size=128):
         for addr in addrs:
             print(f'Loading data from: {addr}')
 
-            # x, bboxes = read_xml(addr)
             x, shapes = read_json(addr)
 
             x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
             x = resize_image(x, final_size)
 
-            # y = generate_dmap(image, bboxes)
-            y, w = generate_seg(x, shapes)
+            y = generate_seg(x, shapes)
 
             x = crop_image(x)
             y = crop_image(y)
-            w = crop_image(w)
 
             data.append([x, y])
 
