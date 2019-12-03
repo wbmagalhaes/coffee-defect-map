eb1760ecc8a03aecb9c5eab9335eb1bd07653f6e
William Bernardes Magalhães <william1504@gmail.com>
2019-12-03 15:28:41 -0300

    resize

--- utils/other.py
+++ utils/other.py
@@ -13,11 +13,12 @@ def clip01(x, y):
     return x, y
 
 
-def resize(dataset, im_size=128):
+def resize(dataset, im_size=256):
     def random_crop(x, y):
         seed = int(random.random() * 1234)
         x = tf.image.random_crop(x, size=[im_size, im_size, 1], seed=seed)
         y = tf.image.random_crop(y, size=[im_size, im_size, 1], seed=seed)
+        return x, y
 
     dataset = dataset.map(random_crop, num_parallel_calls=4)
     return dataset
