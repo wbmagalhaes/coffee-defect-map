fd556d61d882c0c6cde3eec1fc5091e3c28d3176
William <william1504@gmail.com>
2020-07-13 20:50:19 -0300

    dataset

--- create_tfrecord.py
+++ create_tfrecord.py
@@ -26,6 +26,7 @@ if not os.path.isdir(data_dir):
 
 train_path1 = os.path.join(data_dir, 'segmentation_train1.tfrecord')
 train_path2 = os.path.join(data_dir, 'segmentation_train2.tfrecord')
+train_path3 = os.path.join(data_dir, 'segmentation_train3.tfrecord')
 test_path = os.path.join(data_dir, 'segmentation_test.tfrecord')
 
 data = data_reader.load_json(img_dirs, final_size=512)
@@ -40,11 +41,18 @@ test_data = data[train_num:]
 print(f'{len(train_data)} train images.')
 print(f'{len(test_data)} test images.')
 
-train_data1 = train_data[:train_num//2]
-train_data2 = train_data[train_num//2:]
+n = train_num//3
+train_data1 = train_data[:n]
+train_data2 = train_data[n:2*n]
+train_data3 = train_data[2*n:]
+
+print(len(train_data1))
+print(len(train_data2))
+print(len(train_data3))
 
 print('Writing tfrecords...')
 tfrecords.write(train_path1, train_data1)
 tfrecords.write(train_path2, train_data2)
+tfrecords.write(train_path3, train_data3)
 tfrecords.write(test_path, test_data)
 print('Finished.')
