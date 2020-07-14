5bea6f59bd1a0055047681bc50d8cd4a0a8089e8
William <william1504@gmail.com>
2020-07-14 14:28:48 -0300

    dataset

--- create_tfrecord.py
+++ create_tfrecord.py
@@ -24,35 +24,37 @@ training_percentage = 0.8
 if not os.path.isdir(data_dir):
     os.mkdir(data_dir)
 
-train_path1 = os.path.join(data_dir, 'segmentation_train1.tfrecord')
-train_path2 = os.path.join(data_dir, 'segmentation_train2.tfrecord')
-train_path3 = os.path.join(data_dir, 'segmentation_train3.tfrecord')
-test_path = os.path.join(data_dir, 'segmentation_test.tfrecord')
+
+train_path = os.path.join(data_dir, 'segmentation_train')
+teste_path = os.path.join(data_dir, 'segmentation_teste')
+valid_path = os.path.join(data_dir, 'segmentation_valid')
 
 data = data_reader.load_json(img_dirs, final_size=512)
 shuffle(data)
 
 print(f'{len(data)} total images.')
 train_num = int(len(data) * training_percentage)
+teste_num = int(len(data) * (1 - training_percentage)) // 2
 
 train_data = data[:train_num]
-test_data = data[train_num:]
+teste_data = data[train_num:train_num + teste_num]
+valid_data = data[train_num + teste_num:]
 
 print(f'{len(train_data)} train images.')
-print(f'{len(test_data)} test images.')
+print(f'{len(teste_data)} teste images.')
+print(f'{len(valid_data)} valid images.')
 
-n = train_num//3
-train_data1 = train_data[:n]
-train_data2 = train_data[n:2*n]
-train_data3 = train_data[2*n:]
 
-print(len(train_data1))
-print(len(train_data2))
-print(len(train_data3))
+def split_data(path, data, num):
+    size = len(data) // num
+    for i in range(0, num + 1):
+        splitted = data[size * i:size * (i + 1)]
+        if len(splitted) > 0:
+            tfrecords.write(f"{path}_{i}.tfrecords", splitted)
+
 
 print('Writing tfrecords...')
-tfrecords.write(train_path1, train_data1)
-tfrecords.write(train_path2, train_data2)
-tfrecords.write(train_path3, train_data3)
-tfrecords.write(test_path, test_data)
+split_data(train_path, train_data, 4)
+split_data(teste_path, teste_data, 2)
+split_data(valid_path, valid_data, 2)
 print('Finished.')
